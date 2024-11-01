c351be2066fe40e38e0582c78f4e63303b7745f9
vipqua123ok <nnsads005@gmail.com>
2024-11-01 12:34:58 +0700

    Update and rename README.md to .lua

---
+++ .lua
@@ -0,0 +1,14 @@
+
+getgenv().Configs = {
+    ["Team"] = "Pirates",
+    ["FPS Boost"] = true,
+    ["Farm Boss Drops"] = {
+        ["Enable"] = false,
+        ["When x2 Exp Expired"] = false
+    },
+    ["Hop Player Near"] = false,
+    ["Soul Guitar"] = true,
+    ["Find Fruit"] = false, -- Will find 1m+ fruit to unlock swan door to access third sea
+    ["Cursed Dual Katana"] = true
+}
+loadstring(game:HttpGet("https://raw.githubusercontent.com/verudous/Xero-Hub/refs/heads/main/kaitun.lua"))()
--- README.md
+++
@@ -1,1 +0,0 @@
-# lvmaxfull
